d4c2e8a3e8f7d709140fbd794ea31a2c2f676b52
poloyao <78655956@qq.com>
2017-11-27 15:45:10 +0800

    add

--- SqlServer/MSSQLHelper.py
+++ SqlServer/MSSQLHelper.py
@@ -4,7 +4,7 @@ import pymssql
 
 
 class MSSQLHelper:
-    def __init__(self,host,user,pwd,db):
+    def __init__(self, host, user, pwd, db):
         self.host = host
         self.user = user
         self.pwd = pwd
@@ -12,44 +12,26 @@ class MSSQLHelper:
 
     def __GetConnect(self):
         if not self.db:
-            raise(NameError,"没有设置数据库信息")
-        self.conn = pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset="utf8")
+            raise(NameError, "没有设置数据库信息")
+        self.conn = pymssql.connect(
+            host=self.host, user=self.user, password=self.pwd, database=self.db, charset="utf8")
         cur = self.conn.cursor()
         if not cur:
-            raise(NameError,"连接数据库失败")
+            raise(NameError, "连接数据库失败")
         else:
             return cur
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         resList = cur.fetchall()
 
-        #查询完毕后必须关闭连接
+        # 查询完毕后必须关闭连接
         self.conn.close()
         return resList
 
-    def ExecNonQuery(self,sql):
+    def ExecNonQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         self.conn.commit()
         self.conn.close()
-
-# ms = MSSQLHelper(host="192.168.1.1",user="sa",pwd="sa",db="testdb")
-# reslist = ms.ExecQuery("select * from webuser")
-# for i in reslist:
-#     print i
-
-# newsql="update webuser set name='%s' where id=1"%u'测试'
-# print newsql
-# ms.ExecNonQuery(newsql.encode('utf-8'))
-
-# def ShowTable(sqlList,tabHeader):
-#     x = PrettyTable(tabHeader)
-#     #x.padding_width = 3
-        
-#     for i in range(sqlList.__len__()):
-#         x.add_row(sqlList[i])
-
-#     print(x)
-
