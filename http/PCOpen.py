a3711b7a9cacfe8950282844bfd10d0a829ff6dd
poloyao <78655956@qq.com>
2018-01-09 14:55:18 +0800

    增加打卡提醒功能和推送结果判断

--- http/PCOpen.py
+++ http/PCOpen.py
@@ -3,12 +3,40 @@
 import requests
 import datetime
 import time
+import sys
 
 print('正在开机准备中，请勿关闭此界面')
-time.sleep(10)
-now = datetime.datetime.now()
+now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print('系统当前时间为：' + now)
+sckey = 'SCU19816T653afd50681bb2bd5cbd74082c7066a85a544fb059beb'
+#判断是否为打卡期间 则提示打卡
+timeTempMin = '73000'
+timeTempMax = '85000'
+timeMark = datetime.datetime.now().strftime('%H%M%S')
+if timeTempMax > timeMark > timeTempMin:
+    print('提示打卡')
+    postdata = {"text":"已到打卡时间，请确认是否已经打卡成功","desp":now}
+    r = requests.post("https://sc.ftqq.com/" + sckey +".send",data=postdata)
+    if eval(r.text)['errmsg'] == 'success':
+        print('完成推送')
+    else:
+        print('推送失败！！！')
+#time.sleep(10)
+for i in range(10): 
+    sys.stdout.write('|'+'\r') 
+    time.sleep(0.05)
+    sys.stdout.write('/' +'\r') 
+    time.sleep(0.05)
+    sys.stdout.write('-' +'\r') 
+    time.sleep(0.05)
+    sys.stdout.write('\\' +'\r') 
+    time.sleep(0.05)
 postdata = {"text":"单位电脑开机提示","desp":now}
-r = requests.post("https://sc.ftqq.com/SCU19816T7a23e7a4100df029bc8061e0348c0a845a5420ffd26a3.send",data=postdata)
+r = requests.post("https://sc.ftqq.com/" + sckey +".send",data=postdata)
 print(r.text)
-print('完成准备，退出')
+if eval(r.text)['errmsg'] == 'success':
+    print('完成准备，3秒后退出')
+else:
+    print('推送失败！！！3秒后退出')
+time.sleep(3)
+
